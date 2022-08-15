03d2c4171064a31d6336d61c88e778c4fef58e4b
Williams Miguel Ordores Flores <80177649+WilliamsWick@users.noreply.github.com>
2022-08-14 21:46:51 -0500

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: unnecessary_new
+
+import 'package:flutter/material.dart';
+import 'LoginPage.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+    );
+  }
+}
